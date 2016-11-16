ebdb7c9369679e56c5b91f0fb48d6bf810375b2a
Jan-Erik Rediger <janerik@fnordig.de>
2016-11-16 20:37:18 +0100

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+//! Coolify everything
+//!
+//! Example
+//!
+//! ```rust
+//! assert_eq!("cool", coolify::to_cool("rust"));
+//! ```
+
+/// Coolify every value.
 pub fn to_cool<T>(_val: T) -> &'static str {
     "cool"
 }
